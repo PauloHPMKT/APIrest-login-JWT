d441b2ac48b0f2f1f163abf2f6c9fda239c254d4
Paulo Sergio <pauloserg861@gmail.com>
2022-05-29 13:02:59 -0300

    adicionando controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,49 @@
+const userModel = require('../models/UserModel')
 
 const userController = {
     register: function (req, res) {
-        console.log('/register')
-        res.send('register')
+        const {
+            name,
+            email,
+            password,
+        } = req.body
+
+        const user = new userModel({
+            name,
+            email,
+            password,
+        })
+
+        user.save()
+
+        res.send('success')
+    },
+    login: function(req, res) {
+        console.log('login')
+        res.send('login')
+    }
+}
+
+
+module.exports = userController
+
+
+/*
+const userController = {
+    register: async function (req, res) {
+        const user = new UserSchema({
+            name,
+            email,
+            password,
+        } = req.body) 
+
+        try {
+            const savedUser = await user.save()
+            res.send(savedUser)
+        } catch (error) {
+            res.status(400).send(error)
+        }
+
     },
     login: function (req, res) {
         console.log('/login')
@@ -11,4 +52,4 @@ const userController = {
 }
 
 
-module.exports = userController
+module.exports = userController*/
---
+++ models/UserModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+
+const schema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: String,
+    password: Number,
+})
+
+const Model = mongoose.model('UserModel', schema)
+
+module.exports = Model
+
+/*const userSchema = mongoose.Schema({
+    name: { type: String, required: true, minlength: 3, maxlength: 50 },
+    email: { type: String, required: true, minlength: 3, maxlength: 100 },
+    password: { type: String, required: true, minlength: 6, maxlength: 200 },
+    createdAt: { type: Date, default: Date.now }
+})
+
+module.exports = mongoose.model('User', userSchema)*/
